207a3eee904490da5f154d726aa9828d03b5a647
RodrigoEverton1500 <126472645+RodrigoEverton1500@users.noreply.github.com>
2023-12-13 23:49:32 -0300

    Create funções 7.go

---
+++ funções 7.go
@@ -0,0 +1,23 @@
+package main
+import ("fmt"
+"errors")
+
+func f(slice[]int, func()) (erro error) {
+    n, c := 0
+    s := [] int {}
+    s
+    
+    for {
+        fmt.Scan(&n)
+        if n == 0 {
+            break
+        }
+        
+        s[i] = n
+        c++
+    }
+    
+    for i := 0; i < c; i++ {
+        
+    }
+}
